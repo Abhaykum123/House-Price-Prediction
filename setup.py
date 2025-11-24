0f0ae57e0d2f14f853ee010751ef054c68a39cea
Abhaykum123 <anita824122@gmail.com>
2025-11-25 00:14:15 +0530

    add file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abhay Kumar",
+    author_email="abhy8241@gmail.com",
+    packages=find_packages()
+)
